a5700e8b6ba7fcffb8f37d0788ff9c318f419587
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2017-03-05 23:00:19 -0300

    Implementada Aula 2, calculando o IMC do primeiro paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,29 @@
 var titulo = document.querySelector('.titulo')
 titulo.textContent = 'Aparecida Nutricionista'
-console.log(titulo.textContent)
+
+var paciente = document.querySelector('#primeiro-paciente')
+
+var peso = paciente.querySelector('.info-peso').textContent
+var altura = paciente.querySelector('.info-altura').textContent
+
+
+var tdIMC = paciente.querySelector('.info-imc')
+
+var pesoValido = true
+var alturaValida = true
+
+if (peso <= 0 || peso >= 1000) {
+  pesoValido = false
+  tdIMC.textContent = 'Peso Inválido'
+}
+
+if (altura <= 0 || altura >= 4) {
+  alturaValida = false
+  tdIMC.textContent = 'Altura Inválida'
+}
+
+if (pesoValido && alturaValida) {
+  var imc = peso / (altura * altura)
+  tdIMC.textContent = imc
+}
+
